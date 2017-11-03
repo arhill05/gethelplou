e6e1123f6280b3a7d2cbebbdfd278b63b1482b5f
Andrew Hill <AHILL1@maky.midam.farm>
2017-11-03 12:38:31 -0400

    Convert angular-cli to use scss instead of css
    No further comments needed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { DataHttpService } from './dataHttp.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./scss/app.component.scss']
 })
 export class AppComponent {
 
